ec35a2097433abc942591c1234fb1f15192876ee
socrates <natha_ml@icloud.com>
2020-06-12 11:13:29 -0500

    se modifico la conexion para el registro de usuario,Natha

---
+++ scripts/blog.sql
@@ -0,0 +1,95 @@
+CREATE DATABASE project;
+
+USE project;
+/*
+**		Tabla administradores
+*/
+CREATE TABLE `admins` (
+`id` int(99) NOT NULL,
+`Name` varchar(300) NOT NULL,
+`Email` varchar(300) NOT NULL,
+`Password` varchar(450) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `admins`
+ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `admins`
+MODIFY `id` int(99) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+/*
+**		Tabla usuarios
+*/
+CREATE TABLE `turista` (
+`id` int(99) NOT NULL,
+`admin_id` int(99) NOT NULL,
+`Name` varchar(300) NOT NULL,
+`Email` varchar(300) NOT NULL,
+`Password` varchar(450) NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(admin_id) REFERENCES admins(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+ALTER TABLE `turista`
+MODIFY `id` int(99) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+/*
+**		Tabla videojuegos
+*/
+CREATE TABLE `videojuegos` (
+`id` int(99) NOT NULL,
+`turista_id` int(99) NOT NULL,
+`Nombre` varchar(300) NOT NULL,
+`Precio` varchar(300) NOT NULL,
+`consola` varchar(450) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY(turista_id) REFERENCES turista(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+ALTER TABLE `videojuegos`
+MODIFY `id` int(99) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+/*
+**		Tabla animales
+*/
+CREATE TABLE `animales` (
+`id` int(99) NOT NULL,
+`turista_id` int(99) NOT NULL,
+`Nombre` varchar(300) NOT NULL,
+`Genero` varchar(300) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY(turista_id) REFERENCES turista(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+ALTER TABLE `animales`
+MODIFY `id` int(99) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+/*
+**		Tabla país
+*/
+CREATE TABLE `pais` (
+`id` int(99) NOT NULL,
+`turista_id` int(99) NOT NULL,
+`Nombre` varchar(300) NOT NULL,
+`Clima` varchar(300) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY(turista_id) REFERENCES turista(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `pais`
+MODIFY `id` int(99) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+/*
+**		Tabla notas
+*/
+CREATE TABLE `blog` (
+`id` int(99) NOT NULL,
+`turista_id` int(99) NOT NULL,
+`content` varchar(300) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY(turista_id) REFERENCES turista(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `blog`
+MODIFY `id` int(99) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+
+insert into admins values(1, 'Pablo Contreras', 'pablo_contreras_1997@outlook.com', 'Lapatita9');
