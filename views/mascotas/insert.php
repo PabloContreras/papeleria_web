0659c05eeeaadbb9210e2ece1d92700cde8cd26a
RicardoF <rreyesfdz96@gmail.com>
2020-06-13 17:47:45 -0500

    Post Insert turista_id commit

--- views/mascotas/insert.php
+++ views/mascotas/insert.php
@@ -1,10 +1,23 @@
 <?php
 require '/scripts/blog.sql';
+if ($_SERVER['REQUEST_METHOD']== 'POST') {
+    $turista_id = $_POST['turista_id'];
+    $nombre= $_POST['nombre'];
+    $genero= $_POST['genero'];
+try{
+    $conn= new PDO('$servername;$dbname','root','');
 
-$conn = new sqli ($servername,$username,$password,$dbname);
-if ($conn->connect_error) {
-    die ("Conexion Fallida" . $conn->connect_error);
+}catch(PDOExeption $e){
+    echo "Error:" . $e->getMessage(); 
 }
-$insert= "INSERT INTO animales (id,turista_id,Nombre,Genero)";
 
+$statement = $conn->prepare('INSERT INTO mascotas(id,turista_id,Nombre,Genero) VALUES(NUll,:turista_id,:nombre,:genero)');
+$statement->execute(array(
+
+    ':turista_id' => $turista_id,
+    ':nombre'=>$nombre,
+    ':genero'=>$genero
+
+));
+}
 ?>
