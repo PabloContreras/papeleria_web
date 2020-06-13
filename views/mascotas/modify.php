9b898a005f08274b3dece5ca63085292b611883f
RicardoF <rreyesfdz96@gmail.com>
2020-06-13 17:24:15 -0500

    Post Update commit

--- views/mascotas/modify.php
+++ views/mascotas/modify.php
@@ -1,10 +1,21 @@
 <?php
 require '/scripts/blog.sql';
+if ($_SERVER['REQUEST_METHOD']== 'POST') {
+    $id = $_POST['id'];
+    $nombre= $_POST['nombre'];
+    $genero= $_POST['genero'];
 try{
     $conn= new PDO('$servername;$dbname','root','');
 
 }catch(PDOExeption $e){
     echo "Error:" . $e->getMessage(); 
 }
-$statement = $conn->prepare('UPDATE FROM mascotas SET (Nombre,Genero) WHERE id');
+$statement = $conn->prepare('UPDATE FROM mascotas SET (Nombre=:nombre,Genero=:genero) WHERE id=:id');
+$statement->execute(array(
+    ':id'=>$id,
+    ':nombre'=>$nombre,
+    ':genero'=>$genero
+
+));
+}
 ?>
