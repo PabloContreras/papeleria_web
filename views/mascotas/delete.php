4d0f41d47f49382a9a5bcd1cfbf8f8e7d76d44a3
RicardoF <rreyesfdz96@gmail.com>
2020-06-13 17:16:28 -0500

    Post Delete commit

--- views/mascotas/delete.php
+++ views/mascotas/delete.php
@@ -1,10 +1,17 @@
 <?php
 require '/scripts/blog.sql';
+if ($_SERVER['REQUEST_METHOD']== 'POST') {
+    $id = $_POST['id'];
+       
 try{
     $conn= new PDO('$servername;$dbname','root','');
 
 }catch(PDOExeption $e){
     echo "Error:" . $e->getMessage(); 
 }
-$statement = $conn->prepare('DELETE FROM mascotas WHERE id');
+$statement = $conn->prepare('DELETE FROM mascotas WHERE id = :id');
+$statement->execute(array(
+            ':id' => $id
+        ));
+}
 ?>
