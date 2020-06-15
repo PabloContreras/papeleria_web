fa59f860c8002011d024aded757dce3cbe54def7
pablocontreras <pablo_contreras_1997@outlook.com>
2020-06-15 17:14:10 -0500

    home

--- views/videojuegos/index.php
+++ views/videojuegos/index.php
@@ -188,6 +188,16 @@
                 </div>
             </div>
         </div>
+    <footer class="footer fixed-bottom" style="background-color: #1a1a1a;">
+        <div class="row" style="margin-left: 0px; margin-right: 0px;">
+
+            <div class="col-auto ml-auto text-white mb-5 mt-5">
+                &copy;<script>
+                    document.write(new Date().getFullYear());
+                </script>, Hecho con <i class="fa fa-heart heart"></i>
+            </div>
+        </div>
+    </footer>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script type="text/javascript">
